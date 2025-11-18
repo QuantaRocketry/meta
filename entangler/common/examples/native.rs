f3d93c6c1eb52a80efb3758af4ee229c89213e4d
Gibson431 <timmaxgibson@gmail.com>
2025-11-18 17:07:31 +1100

    entangler: Half complete radar display

--- entangler/common/examples/native.rs
+++ entangler/common/examples/native.rs
@@ -8,15 +8,36 @@ use std::{thread, time::Duration};
 fn main() -> Result<(), core::convert::Infallible> {
     let display = SimulatorDisplay::<Rgb565>::new(Size::new(172, 320));
 
-    let output_settings = OutputSettingsBuilder::new().scale(3).build();
+    let output_settings = OutputSettingsBuilder::new().scale(2).build();
     let mut window = Window::new("Entangler", &output_settings);
 
-    let theme = common::DisplayTheme::default();
+    let theme = common::DisplayTheme {
+        primary: Rgb565::BLACK,
+        secondary: Rgb565::CSS_GRAY,
+        background: Rgb565::WHITE,
+    };
     let mut entangler_display = common::Display::new(display, theme, 10);
+
+    let signals = [common::Signal {
+        uid: "VK2-XXX",
+        snr: 10,
+        rssi: -20,
+        angle: 0.0,
+        distance: 325.0,
+    },
+    common::Signal {
+        uid: "OTHER",
+        snr: 10,
+        rssi: -20,
+        angle: -130.0,
+        distance: 1000.0,
+    }];
+
+    entangler_display.select_signal(&signals[0]);
 
     'running: loop {
         let time = Local::now();
-        let _ = entangler_display.draw(time);
+        let _ = entangler_display.draw(&signals);
 
         window.update(entangler_display.get_target());
 
--- entangler/common/src/lib.rs
+++ entangler/common/src/lib.rs
@@ -1,166 +1,235 @@
-use chrono::{Local, Timelike};
-use core::f32::consts::PI;
 use embedded_graphics::{
-    mono_font::{MonoTextStyle, ascii::FONT_9X15},
+    mono_font::{MonoTextStyle, ascii},
     prelude::*,
-    primitives::{Circle, Line, PrimitiveStyle, PrimitiveStyleBuilder, Rectangle},
+    primitives::{Circle, Line, PrimitiveStyle},
     text::Text,
 };
 
 pub struct DisplayTheme<C> {
     pub primary: C,
+    pub secondary: C,
     pub background: C,
 }
 
 impl<C: RgbColor> Default for DisplayTheme<C> {
     fn default() -> Self {
         Self {
-            primary: RgbColor::WHITE,
-            background: RgbColor::BLACK,
+            primary: RgbColor::BLACK,
+            secondary: RgbColor::BLACK,
+            background: RgbColor::WHITE,
         }
     }
 }
 
-pub struct Display<D, C> {
-    target: D,
-    theme: DisplayTheme<C>,
-    clock_face: Circle,
-    margin: u32,
+// Signal of a point of interest
+#[derive(Clone)]
+pub struct Signal<'a> {
+    pub uid: &'a str,
+    pub snr: i32,
+    pub rssi: i32,
+    pub angle: f32,
+    pub distance: f32,
 }
 
-impl<D, C> Display<D, C>
+pub struct Display<'a, D, C> {
+    /// The target to draw to
+    target: D,
+    /// The theme to apply to the display
+    theme: DisplayTheme<C>,
+    /// The margin from the edge of the target in px
+    margin: i32,
+    /// The current POI being tracked
+    selected: Option<&'a str>,
+}
+
+impl<'a, D, C> Display<'a, D, C>
 where
     D: DrawTarget<Color = C>,
     C: PixelColor,
 {
-    pub fn new(target: D, theme: DisplayTheme<C>, margin: u32) -> Self {
-        let bounding_box = target.bounding_box();
-        let diameter = bounding_box.size.width.min(bounding_box.size.height) - 2 * margin;
-        let clock_face = Circle::with_center(bounding_box.center(), diameter);
-
+    pub fn new(target: D, theme: DisplayTheme<C>, margin: i32) -> Self {
         Display {
             target,
             theme,
-            clock_face,
             margin,
+            selected: None,
         }
+    }
+
+    /// Sets the UID of the device being tracked
+    pub fn select_uid(&mut self, uid: &'a str) {
+        self.selected = Some(uid);
+    }
+
+    /// Sets the UID of the device being tracked
+    pub fn select_signal(&mut self, signal: &Signal<'a>) {
+        self.selected = Some(signal.uid);
     }
 
     pub fn get_target(&self) -> &D {
         &self.target
     }
 
-    pub fn draw(&mut self, time: chrono::DateTime<Local>) -> Result<(), D::Error> {
+    pub fn draw(&mut self, signals: &[Signal<'a>]) -> Result<(), D::Error> {
         let _ = self.target.clear(self.theme.background);
 
-        // Calculate the position of the three clock hands in radians.
-        let hours_radians = hour_to_angle(time.hour());
-        let minutes_radians = sexagesimal_to_angle(time.minute());
-        let seconds_radians = sexagesimal_to_angle(time.second());
+        let bounding_box = self.target.bounding_box();
+        let diameter =
+            bounding_box.size.width.min(bounding_box.size.height) - 2 * self.margin as u32;
+        let radar = Circle::with_center(bounding_box.center(), diameter);
+
+        let min_dist = signals
+            .iter()
+            .map(|s| s.distance)
+            .fold(f32::INFINITY, |a, b| a.min(b));
+        let max_dist = signals
+            .iter()
+            .map(|s| s.distance)
+            .fold(0f32, |a, b| a.max(b))
+            * 1.1; // Add some scale to force inside border
 
         // NOTE: In no-std environments, consider using
         // [arrayvec](https://stackoverflow.com/a/39491059/383609) and a fixed size buffer
-        let digital_clock_text = format!(
-            "{:02}:{:02}:{:02}",
-            time.hour(),
-            time.minute(),
-            time.second()
-        );
 
-        self.draw_face()?;
-        self.draw_hand(hours_radians, -60)?;
-        self.draw_hand(minutes_radians, -30)?;
-        self.draw_hand(seconds_radians, 0)?;
-        self.draw_second_decoration(seconds_radians, -20)?;
+        if let Some(signal) = self.selected {
+            for s in signals {
+                if s.uid == signal {
+                    self.draw_uid(signal)?;
+                    self.draw_snr(Some(s.snr))?;
+                    self.draw_rssi(Some(s.rssi))?;
+                    self.draw_signal(radar, max_dist, s, true)?;
+                } else {
+                    self.draw_signal(radar, max_dist, s, false)?;
+                }
+            }
+        } else {
+            self.draw_uid("")?;
+            self.draw_snr(None)?;
+            self.draw_rssi(None)?;
+            for s in signals {
+                self.draw_signal(radar, max_dist, s, false)?;
+            }
+        }
 
-        self.draw_digital_clock(&digital_clock_text)?;
+        radar
+            .into_styled(PrimitiveStyle::with_stroke(self.theme.secondary, 1))
+            .draw(&mut self.target)?;
 
-        // Draw a small circle over the hands in the center of the clock face.
-        // This has to happen after the hands are drawn so they're covered up.
-        Circle::with_center(self.clock_face.center(), 9)
+        Circle::with_center(radar.center(), 6)
             .into_styled(PrimitiveStyle::with_fill(self.theme.primary))
             .draw(&mut self.target)?;
 
         Ok(())
     }
 
-    pub fn draw_hand(&mut self, angle: f32, length_delta: i32) -> Result<(), D::Error> {
-        let end = polar(&self.clock_face, angle, length_delta);
-
-        Line::new(self.clock_face.center(), end)
-            .into_styled(PrimitiveStyle::with_stroke(self.theme.primary, 1))
-            .draw(&mut self.target)
-    }
-
-    /// Draws a circle and 12 graduations as a simple clock face.
-    fn draw_face(&mut self) -> Result<(), D::Error> {
-        // Draw the outer face.
-        self.clock_face
-            .into_styled(PrimitiveStyle::with_stroke(self.theme.primary, 2))
-            .draw(&mut self.target)?;
-
-        // Draw 12 graduations.
-        for angle in (0..12).map(hour_to_angle) {
-            // Start point on circumference.
-            let start = polar(&self.clock_face, angle, 0);
-
-            // End point offset by 10 pixels from the edge.
-            let end = polar(&self.clock_face, angle, -10);
-
-            Line::new(start, end)
-                .into_styled(PrimitiveStyle::with_stroke(self.theme.primary, 1))
-                .draw(&mut self.target)?;
-        }
-
-        Ok(())
-    }
-
-    /// Draws a decorative circle on the second hand.
-    fn draw_second_decoration(&mut self, angle: f32, length_delta: i32) -> Result<(), D::Error> {
-        let decoration_position = polar(&self.clock_face, angle, length_delta);
-
-        let decoration_style = PrimitiveStyleBuilder::new()
-            .fill_color(self.theme.background)
-            .stroke_color(self.theme.primary)
-            .stroke_width(1)
-            .build();
-
-        // Draw a fancy circle near the end of the second hand.
-        Circle::with_center(decoration_position, 11)
-            .into_styled(decoration_style)
-            .draw(&mut self.target)
-    }
-
-    /// Draw digital clock just above center with black text on a white background
-    fn draw_digital_clock(&mut self, time_str: &str) -> Result<(), D::Error> {
-        // Create a styled text object for the time text.
+    fn draw_uid(&mut self, uid: &str) -> Result<(), D::Error> {
+        // Create a styled text object for the id.
         let mut text = Text::new(
-            &time_str,
+            uid,
             Point::zero(),
-            MonoTextStyle::new(&FONT_9X15, self.theme.background),
+            MonoTextStyle::new(&ascii::FONT_7X13, self.theme.primary),
         );
 
-        // Move text to be centered between the 12 o'clock point and the center of the clock face.
+        // Move text to be within margin
         text.translate_mut(
-            self.clock_face.center()
-                - text.bounding_box().center()
-                - self.clock_face.bounding_box().size.y_axis() / 4,
+            text.position
+                + text.bounding_box().size.y_axis() / 2
+                + Point::new(self.margin, self.margin),
         );
-
-        // Add a background around the time digits.
-        // Note that there is no bottom-right padding as this is added by the font renderer itself.
-        let text_dimensions = text.bounding_box();
-        Rectangle::new(
-            text_dimensions.top_left - Point::new(3, 3),
-            text_dimensions.size + Size::new(4, 4),
-        )
-        .into_styled(PrimitiveStyle::with_fill(self.theme.primary))
-        .draw(&mut self.target)?;
-
-        // Draw the text after the background is drawn.
         text.draw(&mut self.target)?;
 
         Ok(())
+    }
+
+    fn draw_snr(&mut self, snr: Option<i32>) -> Result<(), D::Error> {
+        let tag = if let Some(s) = snr {
+            format!("SNR:{}dB", s)
+        } else {
+            format!("SNR:-dB")
+        };
+
+        // Create a styled text object for the id.
+        let mut text = Text::new(
+            &tag,
+            Point::zero(),
+            MonoTextStyle::new(&ascii::FONT_6X13, self.theme.primary),
+        );
+
+        // Move text to be within margin
+        text.translate_mut(
+            self.target
+                .bounding_box()
+                .bottom_right()
+                .ok_or("should have size")
+                .unwrap()
+                - text.bounding_box().size.x_axis()
+                - Point::new(self.margin, self.margin),
+        );
+        text.draw(&mut self.target)?;
+
+        Ok(())
+    }
+
+    fn draw_rssi(&mut self, rssi: Option<i32>) -> Result<(), D::Error> {
+        let tag = if let Some(val) = rssi {
+            format!("RSSI:{}dBm", val)
+        } else {
+            format!("RSSI:-dBm")
+        };
+
+        let mut text = Text::new(
+            &tag,
+            Point::zero(),
+            MonoTextStyle::new(&ascii::FONT_6X13, self.theme.primary),
+        );
+
+        text.translate_mut(
+            self.target.bounding_box().top_left
+                + self.target.bounding_box().size.y_axis()
+                // + text.bounding_box().size.y_axis() / 2
+                + Point::new(self.margin, -self.margin),
+        );
+        text.draw(&mut self.target)?;
+
+        Ok(())
+    }
+
+    fn draw_distance(&mut self, distance: u32) -> Result<(), D::Error> {
+        let tag = format!("{}m", distance);
+        let mut text = Text::new(
+            &tag,
+            Point::zero(),
+            MonoTextStyle::new(&ascii::FONT_6X13, self.theme.primary),
+        );
+
+        text.translate_mut(
+            self.target.bounding_box().top_left
+                + self.target.bounding_box().size.y_axis()
+                // + text.bounding_box().size.y_axis() / 2
+                + Point::new(self.margin, -self.margin),
+        );
+        text.draw(&mut self.target)?;
+
+        Ok(())
+    }
+
+    fn draw_signal(
+        &mut self,
+        radar: Circle,
+        radar_radius: f32,
+        signal: &Signal<'a>,
+        is_selected: bool,
+    ) -> Result<(), D::Error> {
+        let length = signal.distance / radar_radius * (radar.diameter as f32 / 2.0);
+
+        let style = if is_selected {
+            PrimitiveStyle::with_stroke(self.theme.primary, 1)
+        } else {
+            PrimitiveStyle::with_stroke(self.theme.secondary, 1)
+        };
+
+        let line = bearing_to_line(radar.center(), signal.angle, length);
+        line.into_styled(style).draw(&mut self.target)
     }
 }
 
@@ -169,9 +238,7 @@ where
 ///
 /// The angle is relative to the 12 o'clock position and the radius is relative to the edge of the
 /// clock face.
-fn polar(circle: &Circle, angle: f32, radius_delta: i32) -> Point {
-    let radius = circle.diameter as f32 / 2.0 + radius_delta as f32;
-
+fn polar(circle: &Circle, angle: f32, radius: f32) -> Point {
     circle.center()
         + Point::new(
             (angle.sin() * radius) as i32,
@@ -179,18 +246,28 @@ fn polar(circle: &Circle, angle: f32, radius_delta: i32) -> Point {
         )
 }
 
-/// Converts an hour into an angle in radians.
-fn hour_to_angle(hour: u32) -> f32 {
-    // Convert from 24 to 12 hour time.
-    let hour = hour % 12;
-
-    (hour as f32 / 12.0) * 2.0 * PI
+fn bearing_to_line(from: Point, angle: f32, distance: f32) -> Line {
+    Line::new(
+        from,
+        from + Point::new(
+            (angle.to_radians().sin() * distance) as i32,
+            -(angle.to_radians().cos() * distance) as i32,
+        ),
+    )
 }
+
+/// Converts an hour into an angle in radians.
+// fn hour_to_angle(hour: u32) -> f32 {
+//     // Convert from 24 to 12 hour time.
+//     let hour = hour % 12;
+
+//     (hour as f32 / 12.0) * 2.0 * PI
+// }
 
 /// Converts a sexagesimal (base 60) value into an angle in radians.
-fn sexagesimal_to_angle(value: u32) -> f32 {
-    (value as f32 / 60.0) * 2.0 * PI
-}
+// fn sexagesimal_to_angle(value: u32) -> f32 {
+//     (value as f32 / 60.0) * 2.0 * PI
+// }
 
 #[cfg(test)]
 mod tests {
